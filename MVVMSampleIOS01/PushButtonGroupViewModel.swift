4d9df17c0a057e805bc3aad96c7ac2f7324cc4c3
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-10-05 23:04:27 +0900

    ・ViewModel(PushButtonGroupViewModel)を実装する。

---
+++ MVVMSampleIOS01/PushButtonGroupViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  PushButtonGroupViewModel.swift
+//  MVVMSampleIOS01
+//
+//  Created by velo.yamigiku on 2020/10/05.
+//  Copyright © 2020 velo.yamigiku. All rights reserved.
+//
+
+import RxSwift
+
+class PushButtonGroupViewModel: NSObject {
+    
+    private var pushButtonGroup: PushButtonGroup
+    
+    init(rowCount: Int, colCount: Int) {
+        pushButtonGroup = PushButtonGroup(
+            rowCount: rowCount,
+            colCount: colCount)
+    }
+    
+    func getButtonOnOff(buttonNumber: Int) -> Observable<Bool> {
+        return pushButtonGroup.getButtonOnOff(buttonNumber: buttonNumber)
+    }
+    
+    func getIsAllOn() -> Observable<Bool> {
+        return pushButtonGroup.isAllOn
+    }
+    
+    func pushButton(buttonNumber: Int) {
+        pushButtonGroup.push(buttonNumber: buttonNumber)
+    }
+    
+}
