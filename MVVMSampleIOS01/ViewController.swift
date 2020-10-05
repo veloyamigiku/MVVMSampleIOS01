d2ef3ead65726f3dec750c3c802ff9329509fed5
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-10-05 23:05:32 +0900

    ・View(ViewController)とViewModel(PushButtonGroupViewModel)を連携する。

--- MVVMSampleIOS01/ViewController.swift
+++ MVVMSampleIOS01/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
+    private var disposeBag: DisposeBag!
+    private var pbgVm: PushButtonGroupViewModel!
+    private var buttonAry: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        disposeBag = DisposeBag()
+        pbgVm = PushButtonGroupViewModel(
+            rowCount: 3,
+            colCount: 3)
+        buttonAry = [
+            button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
+            button8,
+            button9
+        ]
+        bindWithViewModel()
+        
+    }
+    
+    func bindWithViewModel() {
+        
+        for (index, button) in buttonAry.enumerated() {
+            
+            button.rx.tap.subscribe(onNext: { () in
+                self.pbgVm.pushButton(buttonNumber: index + 1)
+            })
+            .disposed(by: disposeBag)
+            
+            pbgVm.getButtonOnOff(buttonNumber: index + 1).subscribe(onNext: { onOff in
+                if onOff {
+                    button.backgroundColor = UIColor.red
+                } else {
+                    button.backgroundColor = UIColor.gray
+                }
+            })
+            .disposed(by: disposeBag)
+            
+        }
+        
+        pbgVm.getIsAllOn().subscribe(onNext: { isAllOn in
+            if isAllOn {
+                print("isAllOn:", isAllOn)
+            }
+        })
+        .disposed(by: disposeBag)
+        
     }
 
-
 }
-
